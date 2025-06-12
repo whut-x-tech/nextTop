67eb913cd12902717ff2db923c4c81e974c76e8f
dinghaoran02 <dinghaoran02@meituan.com>
2025-06-12 17:32:03 +0800

    init project

---
+++ src/main/java/top/liuqiao/nextTop/NextTopApplication.java
@@ -0,0 +1,13 @@
+package top.liuqiao.nextTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextTopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextTopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/liuqiao/nextTop/NextTopApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liuqiao.nextTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextTopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
