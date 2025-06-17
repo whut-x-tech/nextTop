5d188c72d136ebdb6abc4a0d1c5ea33429ca8c4e
liuqiao <chengxuyuan2023@qq.com>
2025-06-17 12:47:24 +0800

    添加参数绑定错误处理器

--- src/main/java/top/liuqiao/nextTop/exception/GlobalExceptionHandler.java
+++ src/main/java/top/liuqiao/nextTop/exception/GlobalExceptionHandler.java
@@ -1,14 +1,19 @@
 package top.liuqiao.nextTop.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.oxm.ValidationFailureException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import top.liuqiao.nextTop.common.BaseResponse;
 import top.liuqiao.nextTop.common.ErrorCode;
 import top.liuqiao.nextTop.common.ResultUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -28,6 +33,20 @@ public class GlobalExceptionHandler {
             builder.append(":");
             builder.append(fieldError.getRejectedValue());
             builder.append(" 参数错误\n");
+        }
+        return ResultUtils.error(ErrorCode.PARAMS_ERROR, builder.toString());
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public BaseResponse<?> validationExceptionHandler(HandlerMethodValidationException e) {
+        log.error("validationException", e);
+        final List<ParameterValidationResult> parameterValidationResultList = e.getAllValidationResults();
+        final StringBuilder builder = new StringBuilder(64);
+        for (ParameterValidationResult parameterValidationResult : parameterValidationResultList) {
+            for (MessageSourceResolvable resolvableError : parameterValidationResult.getResolvableErrors()) {
+                builder.append(resolvableError.getDefaultMessage());
+                builder.append("\n");
+            }
         }
         return ResultUtils.error(ErrorCode.PARAMS_ERROR, builder.toString());
     }
